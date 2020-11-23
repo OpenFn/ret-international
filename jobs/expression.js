b863cb7f76f1025d55f02e1b1cea2ea49ee15f49
Berian <chaiwa.berian@gmail.com>
2020-11-23 12:57:10 +0200

    Update expression.js

--- jobs/expression.js
+++ jobs/expression.js
@@ -1,4 +1,32 @@
-// language-http
-
-get('someUrl', { ...params }, callback);
-post('someUrl', { body: state => state.data })'
+{
+  "configuration": {
+   
+  },
+  "data": {
+    "__query_params": {},
+    "body": {
+      "Age": "25",
+      "First_Name": "Felix",
+      "Gender": "male",
+      "Last_Name": "Phiri",
+      "Services": "service_1 service_2 service_4",
+      "__version__": "vwrdjsTYRoHAJxbZWnCcor",
+      "_attachments": [],
+      "_geolocation": [null, null],
+      "_id": 75126512,
+      "_notes": [],
+      "_status": "submitted_via_web",
+      "_submission_time": "2020-11-20T11:18:19",
+      "_submitted_by": null,
+      "_tags": [],
+      "_uuid": "2e512959-7e18-476f-b420-96362fea69a1",
+      "_validation_status": {},
+      "_xform_id_string": "aMiEiFjejFkpRLuc3krcvp",
+      "end": "2020-11-20T13:18:08.687+02:00",
+      "formhub/uuid": "10f8e684befa4bd284ee05ff48dd62c8",
+      "meta/instanceID": "uuid:2e512959-7e18-476f-b420-96362fea69a1",
+      "start": "2020-11-20T13:17:28.494+02:00"
+    },
+    "form": "beneficiary_registration"
+  }
+}
