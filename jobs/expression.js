763c7b39cce9ed4c1a486c5f7315f916e0640c34
Berian <chaiwa.berian@gmail.com>
2020-11-24 04:06:55 +0200

    Fixes #1
    This fixes #1 for now until we have better helpers for null-ability checks. Is there a way we can have libraries like Lodash exposed for us to use when writing jobs? Or wrapper functions we can use for quick validations such as object/array null-ability checks? @taylordowns2000 , @lakhassane @aleksa-krolls

--- jobs/expression.js
+++ jobs/expression.js
@@ -1,19 +1,72 @@
-upsert('beneficiary_registration', 'formhub_uuid', {
-  xform_id_string: state.data.body._xform_id_string,
-  age: state.data.body.Age,
-  first_name: state.data.body.First_Name,
-  gender: state.data.body.Gender,
-  last_name: state.data.body.Last_Name,
-  services: `{${state.data.body.Services}}`,
-  version: state.data.body.__version__,
-  id: state.data.body._id,
-  status: state.data.body._status,
-  submission_time: state.data.body._submission_time,
-  submitted_by: state.data.body._submitted_by,
-  uuid_column: state.data.body._uuid,
-  validation_status: state.data.body._validation_status,
-  end_time: state.data.body.end,
-  formhub_uuid: state.data.body['formhub/uuid'],
-  meta_instance_id: state.data.body['meta/instanceID'],
-  start_time: state.data.body.start,
+alterState(state => {
+  function clean(obj) {
+    for (var propName in obj) {
+      if (obj[propName] === '-' || obj[propName] === null) {
+        delete obj[propName];
+      }
+    }
+  }
+  
+function manualFieldExclude(){
+  state.data.body._attachments = null;
+  state.data.body._geolocation = null;
+  state.data.body._notes = null;
+  state.data.body._submitted_by= null;
+  state.data.body._tags= null;
+  state.data.body._validation_status= null;
+}
+  
+const genderTransformations = {
+  'female': 'f',
+  'male': 'm'
+};
+
+function transformGender(value){
+  let gender = genderTransformations[value.toString().toLowerCase()];
+  state.data.body.gender = gender? gender: null;
+}
+
+const keyMappings = {
+  "Age": "age",
+  "First_Name": "first_name",
+  "Gender": "gender",
+  "Last_Name": "last_name",
+  "Services": "services",
+  "end": "_end",
+  "formhub/uuid": "_formhub_uuid",
+  "meta/instanceID": "_meta_instance_id",
+  "start": "_start"
+};
+
+function mapSourceToDestinationFields(obj, keyMap){
+  for(var [old_key, new_key] of Object.entries(keyMap)){
+    if(obj[old_key]){
+      obj[new_key] = obj[old_key];
+      delete obj[old_key];
+    }
+  }
+}
+
+// step 1: map source/destination keys
+mapSourceToDestinationFields(state.data.body, keyMappings);
+
+// step 2: transform data
+transformGender(state.data.body.gender);
+
+// step 3: exclude null fields, manually, until we have built
+// better helpers for nullability checks/type casting
+manualFieldExclude();
+
+// step 4: clean data for insert payload
+clean(state.data.body);
+
+  return state;
 });
+
+// step 4: insert data
+upsert(
+  'beneficiary_registration',
+  '_id',
+  state.data.body,
+  {writeSql:true, execute: true}
+);
